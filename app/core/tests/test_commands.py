2e85f10615a85e4c5d6b7d6d2065e34124f1b629
newjan <newjan.axios@lendenclub.com>
2023-03-04 22:25:22 +0545

    009 Add wait for db command

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -2,8 +2,6 @@
 Test custom Django management commands.
 """
 from unittest.mock import patch
-from django.core.checks import database
-
 from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
@@ -18,20 +16,18 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
-        
+
         call_command('wait_for_db')
-        
-        patched_check.assert_called_once_with(database=['default'])
-        
+
+        patched_check.assert_called_once_with(databases=['default'])
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-            
-        call_command('wait_for_db')
-        
-        self.assert_equal(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
 
- 
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
